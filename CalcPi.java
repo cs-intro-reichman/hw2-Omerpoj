1ede0cb5d2f030b7c747072a92a8939a3f98812e
עומר פוזזינסקי <omer_pojejinsky@wmrs-MBP.lan>
2024-11-14 18:51:15 +0200

    3

--- CalcPi.java
+++ CalcPi.java
@@ -3,7 +3,7 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int num = Integer.parseInt(args[0]);
 		double pi = 1;
-		for(int i = 1;i<num-1;i++){
+		for(int i = 0;i<num-1;i++){
 			if (i % 2 == 0) {
 				pi-=((double)1/(3+(2*i)));
 			}
